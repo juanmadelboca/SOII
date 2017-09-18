bde4ac5d0309243e57c668511bb02fa6fd2ac733
juanmadelboca <juanmadelboca@gmail.com>
2017-09-18 19:46:12 -0300

    addding www to html path

--- TP3/src/scripts/init.sh
+++ TP3/src/scripts/init.sh
@@ -2,7 +2,7 @@ sudo apt-get install lighttpd
 chown www-data /var/www/html
 echo "www-data ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 sudo cp -r /home/pi/GIT/SOII/src/html /var/www/html
-cd /var/html/cgi-bin
+cd /var/www/html/cgi-bin
 make
 cd ..
 sudo lighty-enable-mode cgi
