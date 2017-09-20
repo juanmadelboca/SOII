3e602c87876bc867413f7617f5c4838a9fa61680
juanmadelboca <juanmadelboca@gmail.com>
2017-09-20 17:30:09 -0300

    fixing warnings

--- TP3/src/html/cgi-bin/delete.c
+++ TP3/src/html/cgi-bin/delete.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void setWeb(void);
+void setWeb(char *);
 
 int main(void)
 {
 	char *data;
 	char module[50];
+	char message[50];
 	data = getenv("QUERY_STRING");
-	sscanf(data,"module=%s",&module);
+	sscanf(data,"module=%s",module);
 	char command[200]="sudo rmmod ";
 	strcat(command,module);
 	fflush(0);
-	system(command);
-	setWeb();
+	system(command);	
+	strcpy(message,"El modulo ha sido removido");
+	
+	setWeb(message);
 	return 0;
 }
-void setWeb(){
+void setWeb(char * message){
 /**
 @brief provee el codigo html para que el cliente visualice la pagina
 **/
@@ -26,7 +29,7 @@ printf("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\
 printf("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\"");
 printf("integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
 printf("<link rel=\"stylesheet\" type=\"text/css\" href=\"./../css/main.css\" media=\"screen\" /></head><body><div class=\"main-container text-align\">");
-printf("<div>El modulo ha sido removido</div>");
+printf("<div>%s </div>",message);
 printf("<form enctype=\"multipart/form-data\" action=\"upload.cgi\" method=\"POST\" class=\"row\" ><div class=\"col-md-6\">");
 printf("<label class=\"btn btn-grey-form\">Buscar <input type=\"file\" name=\"upload\" style=\"display: none;\"></label></div>");
 printf("<div class=\"col-md-6\"><input type=\"submit\" value=\"Subir\" class=\"btn btn-grey-form\" /></div></form>");
