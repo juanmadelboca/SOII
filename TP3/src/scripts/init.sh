c26793fabe12dd2324d12f75372bf37fd4fefc59
juanmadelboca <juanmadelboca@gmail.com>
2017-09-20 00:22:30 -0300

    updating project

--- TP3/src/scripts/init.sh
+++ TP3/src/scripts/init.sh
@@ -1,7 +1,7 @@
 sudo apt-get install lighttpd
 chown -R www-data /var/www/html
 echo "www-data ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-sudo cp -r /home/pi/GIT/SOII/TP3/src/var-www-html /var/www/
+sudo cp -R /home/pi/GIT/SOII/TP3/src/html /var/www
 cd /var/www/html/cgi-bin
 make
 cd ..
